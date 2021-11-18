f5afadd5950cb4f6271511550056cce058ae6090
Gubastaya Panda <v.paksha@gaskar.group>
2021-11-18 10:07:23 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [neuroioData, setNeuroioData] = useState({
+    result: 'exact',
+    pid: '1273ee8b-896f-4721-8f07-bfbe06cd11af',
+    age: null,
+    sex: null,
+    mood: null,
+    confidence: 97.89,
+    pan: 4.98,
+    tilt: 2.96,
+  });
+
+  const onImageChange = event => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
+
+  const fetchNeuroio = async () => {
+    const response = await axios({
+      method: 'post',
+      url: 'https://api.neuroio.com/v1/persons/search/',
+      data: {
+        image,
+      },
+    });
+    console.log(response);
+  };
+
+  const onButtonClick = () => {
+    console.log('onButtonClick');
+    fetchNeuroio();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="HalfWidth">
+        <input type="file" onChange={onImageChange} className="filetype" />
+        {image && (
+          <button className="ImageSendBtn" onClick={onButtonClick}>
+            Отправить фото в neuroio
+          </button>
+        )}
+        <img src={image} alt="Загрузите фото сотрудника" />
+      </div>
+
+      <div className="HalfWidth">
+        <div>
+          <pre>{JSON.stringify(neuroioData, null, 2)}</pre>
+        </div>
+      </div>
     </div>
   );
 }
